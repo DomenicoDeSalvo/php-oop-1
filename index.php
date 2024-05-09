e6f7e22e14ddfc8b0aa30b80c3fa832900414ee2
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-09 15:59:48 +0200

    Added methods to acquire data

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <!-- PHP -->
 <?php
+//CLASSE
 class Production{
     public $title;
     public $language;
     public $rate;
+    //COSTRUTTORE
+    function __construct($_title, $_language, $_rate ) {
+        $this -> setTitle($_title);
+        $this -> setLanguage($_language);
+        $this -> setRate($_rate);
+    }
+    //ACQUISIZIONE TITOLO
+    public function setTitle(string $new_title) : void{
+        $this -> $title = $new_title;
+    }
 
-    function __construct($_title, $_language, $rate ) {
-        $this => $title = $_title;
-        $this => $language = $_language;
-        $this => $rate = $_rate;
+    //ACQUISIZIONE LINGUA
+    public function setLanguage(string $new_language) : void{
+        $this -> $language = $new_language;
+    }
+
+    //ACQUISIZIONE VOTO
+    public function setRate(int $new_rate) : void{
+        if(is_numeric($new_rate) && $new_rate > 0 && $new_rate <11){
+            $this -> $rate = intval($new_rate);
+        }
     }
 }
 ?>
