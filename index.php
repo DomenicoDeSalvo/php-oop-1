3036f3df8ad5fab1eecb1a9901379a13e3c98155
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-09 20:02:41 +0200

    Movies are now printed correctly

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ class Production{
             $this -> rate = intval($new_rate);
         }
     }
+
 }
 
 //CLASSE GENRE
@@ -52,10 +53,17 @@ class Genre {
     }
 }
 
-$movie1 = new Production ('La Febbra','Italiano', 10, new Genre ('Thriller',2004) );
-var_dump($movie1);
-$movie2 = new Production ('Avatar 2', 'Inglese', 8, new Genre ('Fantascienza',2022));
-var_dump($movie2);
+$movies = array(
+    new Production ('La Febbra','Italiano', 10, new Genre ('Drammatico',2004)),
+    new Production ('Avatar 2', 'Inglese', 8, new Genre ('Fantascienza',2022)),
+    new Production ('Dunkirk', 'Inglese', 9, new Genre ('Drammatico',2017)),
+    new Production ('Il ragazzo e l\'airone', 'Giapponese', 8, new Genre ('Fantastico',2023)),
+    new Production ('Good Bye, Lenin!', 'Tedesco', 8, new Genre ('Drammatico',2003)),
+    new Production ('Il mio amico Robot', 'Muto', 7, new Genre ('Musical',2023)),
+    new Production ('Civil War', 'Inglese', 7, new Genre ('Drammatico',2024)),
+    new Production ('Mobbasta veramente però', 'Italiano', 10, new Genre ('Drammatico',2004)),
+    new Production ('Grazie Ragazzi', 'Italiano', 7, new Genre ('Commedia',2023)),
+);
 
 ?>
 
@@ -66,8 +74,43 @@ var_dump($movie2);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP OOP</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-    
+        <div class="container">
+            <div class="row row-cols-3">
+                <?php
+                    foreach ($movies as $movie) {
+                        ?>
+                        <div class="col p-2">
+                            <div class="card" >
+                                <div class="card-body">
+                                    <h5 class="text-center card-title"><?php echo $movie->title?></h5>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <span class="fw-bold">Lingua: </span> 
+                                            <?php echo $movie->language?>
+                                        </li>
+                                        <li>
+                                            <span class="fw-bold">Voto: </span> 
+                                            <?php echo $movie->rate?>
+                                        </li>
+                                        <li>
+                                            <span class="fw-bold">Genere: </span> 
+                                            <?php echo $movie->genre->name?>
+                                        </li>
+                                        <li>
+                                            <span class="fw-bold">Anno: </span> 
+                                            <?php echo $movie->genre->year?>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    };
+                ?>
+            </div>
+        </div>
     </body>
 </html>
