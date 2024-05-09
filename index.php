04c0d46de55b365a4068e502972621d4abf25243
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-09 16:14:30 +0200

    Added 2 movies

--- index.php
+++ index.php
@@ -6,28 +6,34 @@ class Production{
     public $language;
     public $rate;
     //COSTRUTTORE
-    function __construct($_title, $_language, $_rate ) {
+    function __construct(string $_title, string $_language, int $_rate ) {
         $this -> setTitle($_title);
         $this -> setLanguage($_language);
         $this -> setRate($_rate);
     }
     //ACQUISIZIONE TITOLO
     public function setTitle(string $new_title) : void{
-        $this -> $title = $new_title;
+        $this -> title = $new_title;
     }
 
     //ACQUISIZIONE LINGUA
     public function setLanguage(string $new_language) : void{
-        $this -> $language = $new_language;
+        $this -> language = $new_language;
     }
 
     //ACQUISIZIONE VOTO
     public function setRate(int $new_rate) : void{
         if(is_numeric($new_rate) && $new_rate > 0 && $new_rate <11){
-            $this -> $rate = intval($new_rate);
+            $this -> rate = intval($new_rate);
         }
     }
 }
+
+$movie1 = new Production ('La Febbra','Italiano',10);
+var_dump($movie1);
+$movie2 = new Production ('Il vecchio Conio', 'Italiano', 9);
+var_dump($movie2);
+
 ?>
 
 <!-- HTML -->
